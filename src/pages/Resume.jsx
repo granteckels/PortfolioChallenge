e3e1241bd08ba5854e0053738d141cff705d8df1
granteckels <geckels1@gmail.com>
2025-03-19 22:50:06 -0500

    Finish Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,21 @@
 function Resume() {
     return (
-        <p>Resume</p>
+        <>
+            <h2>Resume</h2>
+            <p>Download my <a href="">resume</a></p>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>NodeJS</li>
+                <li>ExpressJS</li>
+                <li>PostgreSQL</li>
+            </ul>
+        </>
     )
 }
 
