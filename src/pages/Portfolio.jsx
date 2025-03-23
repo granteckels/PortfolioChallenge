4938b72ecb38413f41c19a2a704e1fd2b67844c6
granteckels <geckels1@gmail.com>
2025-03-22 22:06:10 -0500

    Change links for portfolio images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,17 +1,24 @@
 import '/src/assets/Portfolio.css';
 import Project from "/src/components/Project.jsx"
 
+import Calculator from '../assets/calculator.webp';
+import LEDWall from '../assets/led-wall.jpg';
+import PastelPuzzles from '../assets/pastel-puzzles.webp';
+import RunBuddy from '../assets/run-buddy.png';
+import SurfReport from '../assets/surf-report.jpg';
+import Microblog from '../assets/microblog.webp';
+
 function Portfolio() {
     return (
         <>
             <h2>Portfolio</h2>
             <ul id="projects">
-                <Project src='/src/assets/calculator.webp' name="Calculator" deployLink="" repoLink="" />
-                <Project src='/src/assets/led-wall.jpg' name="LED Wall" deployLink="" repoLink="" />
-                <Project src='/src/assets/pastel-puzzles.webp' name="Pastel Puzzles" deployLink="" repoLink="" />
-                <Project src='/src/assets/run-buddy.png' name="Run Buddy" deployLink="" repoLink="" />
-                <Project src='/src/assets/surf-report.jpg' name="Surf Report" deployLink="" repoLink="" />
-                <Project src='/src/assets/microblog.webp' name="Microblog" deployLink="" repoLink="" />
+                <Project src={Calculator} name="Calculator" deployLink="" repoLink="" />
+                <Project src={LEDWall} name="LED Wall" deployLink="" repoLink="" />
+                <Project src={PastelPuzzles} name="Pastel Puzzles" deployLink="" repoLink="" />
+                <Project src={RunBuddy} name="Run Buddy" deployLink="" repoLink="" />
+                <Project src={SurfReport} name="Surf Report" deployLink="" repoLink="" />
+                <Project src={Microblog} name="Microblog" deployLink="" repoLink="" />
             </ul>
         </>
     )
